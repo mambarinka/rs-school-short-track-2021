2d9deb4f32dda19bec1e603e4c006d0d1fb6c2e8
Anna <mambarinka@mail.ru>
2021-04-09 16:05:55 +0400

    feat: implements 04-mine-sweeper.js

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,91 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = JSON.parse(JSON.stringify(matrix));
+
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result[i].length; j++) {
+      result[i][j] = 0;
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === true) {
+        switch (i) {
+          case 0:
+            switch (j) {
+              case 0:
+                result[i][j + 1]++;
+                result[i + 1][j]++;
+                result[i + 1][j + 1]++;
+                break;
+              case result[i].length - 1:
+                result[i][j - 1]++;
+                result[i + 1][j]++;
+                result[i + 1][j - 1]++;
+                break;
+              default:
+                result[i][j - 1]++;
+                result[i][j + 1]++;
+                result[i + 1][j]++;
+                result[i + 1][j - 1]++;
+                result[i + 1][j + 1]++;
+            }
+            break;
+          case result.length - 1:
+            switch (j) {
+              case 0:
+                result[i][j + 1]++;
+                result[i - 1][j]++;
+                result[i - 1][j + 1]++;
+                break;
+              case result[i].length - 1:
+                result[i][j - 1]++;
+                result[i - 1][j]++;
+                result[i - 1][j - 1]++;
+                break;
+              default:
+                result[i][j - 1]++;
+                result[i][j + 1]++;
+                result[i - 1][j]++;
+                result[i - 1][j - 1]++;
+                result[i - 1][j + 1]++;
+            }
+            break;
+          default:
+            switch (j) {
+              case 0:
+                result[i - 1][j]++;
+                result[i][j + 1]++;
+                result[i + 1][j]++;
+                result[i - 1][j + 1]++;
+                result[i + 1][j + 1]++;
+                break;
+              case result[i].length - 1:
+                result[i - 1][j]++;
+                result[i][j - 1]++;
+                result[i + 1][j]++;
+                result[i - 1][j - 1]++;
+                result[i + 1][j - 1]++;
+                break;
+              default:
+                result[i + 1][j]++;
+                result[i][j + 1]++;
+                result[i][j - 1]++;
+                result[i - 1][j]++;
+                result[i - 1][j - 1]++;
+                result[i - 1][j + 1]++;
+                result[i + 1][j - 1]++;
+                result[i + 1][j + 1]++;
+            }
+            break;
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = minesweeper;
