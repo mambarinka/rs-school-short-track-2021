31799e01dcde036c45ecadd486fabc0170423ca6
Anna <mambarinka@mail.ru>
2021-04-11 17:14:01 +0400

    feat: implements 15-get-email-domain.js

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,10 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const regex = /@[-\w]+\.\w+/gm;
+  const result = email.match(regex);
+  return String(result).slice(1);
 }
 
 module.exports = getEmailDomain;
