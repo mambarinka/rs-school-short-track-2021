7ccf6b2291415a44ea686ff93f19d6983ee25bed
Anna <mambarinka@mail.ru>
2021-04-11 13:52:41 +0400

    feat: implements 06-mac-address.js

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,9 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const reg = /[A-F0-9]{2}-[A-F0-9]{2}-[A-F0-9]{2}-[A-F0-9]{2}-[A-F0-9]{2}-[A-F0-9]{2}/.test(n);
+  return reg;
 }
 
 module.exports = isMAC48Address;
